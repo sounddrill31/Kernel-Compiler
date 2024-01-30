a246f9a051b354e46463bfb19fba197f0f54c592
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-01-30 15:52:00 +0530

    Give run.sh execute permissions

--- clone.sh
+++ clone.sh
@@ -7,4 +7,5 @@ git clone --depth=1 https://github.com/kdrag0n/proton-clang CLANG-13
 
 # Start Build
 wget https://github.com/sounddrill31/Kernel-Compiler/raw/master/run.sh
+chmod +x run.sh
 bash run.sh
